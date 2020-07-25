f65b6d83c57f62a250e70a3c632668dde8efed9c
dan4inatora <jtolev@astea.net>
2020-07-25 13:23:29 +0300

    last commit

--- client/src/components/bookView/bookComponent.jsx
+++ client/src/components/bookView/bookComponent.jsx
@@ -15,27 +15,27 @@ class bookComponent extends Component {
     return (
     <div className="bookContainer">{this.props.book !== undefined ? 
       <div>
-      <div className="picture">
-        <img src={`${backend}${this.props.book.imagePath}`} alt={`${book.name}`} width="600" height="400"/>
-      </div>
-      <div className="bookname">
-        <h4>Title</h4>
-        <p>{this.props.book.name}</p>
-      </div>
-      <div className="bookdesc">
-        <h4>Description</h4>
-        <div>{this.props.book.description}</div>
-      </div>
-      <div className="bookRating">
-        <p>{getAvg(book)}</p>
-      </div>
-      <div className="Comments">
-          <h4>Comments</h4>
-          <div>{book.comments_id.map(comment => (
-            <p>{comment}</p>
-          ))}</div>
-      </div>
-    </div> 
+          <div className="picture">
+            <img src={`${backend}${this.props.book.imagePath}`} alt={`${book.name}`} width="600" height="400"/>
+          </div>
+          <div className="bookname">
+            <h4>Title</h4>
+            <p>{this.props.book.name}</p>
+          </div>
+          <div className="bookdesc">
+            <h4>Description</h4>
+            <div>{this.props.book.description}</div>
+          </div>
+          <div className="bookRating">
+            <p>{getAvg(book)}</p>
+          </div>
+          <div className="Comments">
+              <h4>Comments</h4>
+              <div>{book.comments_id.map(comment => (
+                <p>{comment}</p>
+              ))}</div>
+          </div>
+      </div> 
       
     :<div></div>}</div>
 
